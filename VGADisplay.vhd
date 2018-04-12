d470ce0f53b4463c7d00d289a9779bf763845c1b
Bartosz Rodziewicz <bartoszka1996@gmail.com>
2018-04-12 16:29:15 +0200

    Fix formatting and intends in VGADisplay.vhd

--- VGADisplay.vhd
+++ VGADisplay.vhd
@@ -12,11 +12,11 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity VGADisplay is
 	Port ( Clk_50MHz : in  STD_LOGIC;
-		   VGA_R : out  STD_LOGIC;
-		   VGA_G : out  STD_LOGIC;
-		   VGA_B : out  STD_LOGIC;
-		   VGA_HS : out  STD_LOGIC;
-		   VGA_VS : out  STD_LOGIC);
+			VGA_R : out  STD_LOGIC;
+			VGA_G : out  STD_LOGIC;
+			VGA_B : out  STD_LOGIC;
+			VGA_HS : out  STD_LOGIC;
+			 VGA_VS : out  STD_LOGIC);
 end VGADisplay;
 
 architecture Behavioral of VGADisplay is
@@ -28,21 +28,21 @@ begin
 	begin
 		if (rising_edge(Clk_50MHz)) then
 			if (hs_counter < -64) then
-            VGA_HS <= '0';
-         else
-            VGA_HS <= '1';
-         end if;
+				VGA_HS <= '0';
+			else
+				VGA_HS <= '1';
+			end if;
 		end if;
 	end process;
-   
+
 	Vertical_sync : process ( Clk_50MHz, vs_counter ) is
 	begin
 		if (rising_edge(Clk_50MHz)) then
 			if (vs_counter < -23) then
-            VGA_VS <= '0';
-         else
-            VGA_VS <= '1';
-         end if;
+				VGA_VS <= '0';
+			else
+				VGA_VS <= '1';
+			end if;
 		end if;
 	end process;
 
@@ -61,8 +61,8 @@ begin
 			end if;
 		end if;
 	end process;
-	
-		Pixel_search : process ( vs_counter, hs_counter ) is
+
+	Pixel_search : process ( vs_counter, hs_counter ) is
 	begin
 		if (hs_counter > 416 and hs_counter < 458 and vs_counter > 278 and vs_counter < 320) then
 			VGA_R <= '1';
@@ -76,4 +76,3 @@ begin
 	end process;
 
 end Behavioral;
-
